716005a528fba578042417449cb3d36c1a9c0a1a
Corina <v-corgum@microsoft.com>
2019-05-16 16:19:03 -0700

    Updates

---
+++ precache-manifest.2d6625f10eebd177ccff6bd7b129f343.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77de6de27f1c24d7514111d31e901f0c",
+    "url": "/ARIA-screenreader-testing/index.html"
+  },
+  {
+    "revision": "9747810f88388fdafc8f",
+    "url": "/ARIA-screenreader-testing/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/ARIA-screenreader-testing/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "9747810f88388fdafc8f",
+    "url": "/ARIA-screenreader-testing/static/js/main.7b2f6d39.chunk.js"
+  },
+  {
+    "revision": "fd008ffe58ab7ae6042f",
+    "url": "/ARIA-screenreader-testing/static/js/runtime~main.b48f5166.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/ARIA-screenreader-testing/precache-manifest.2d6625f10eebd177ccff6bd7b129f343.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ARIA-screenreader-testing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
